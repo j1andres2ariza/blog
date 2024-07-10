33d36c91115be28a6db5c9caebe8c1e6bdc46396
Juan <j1andres2ariza@gmail.com>
2024-07-10 11:47:49 -0500

    Clase 4

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return view('posts.index');
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function show($post)
+    {
+
+        //compact('post');  ['post' => $post]
+
+        return view('posts.show', [
+            'post'=> $post
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', HomeController::class);
+
+Route::get('/posts', [PostController::class,'index']);
+
+Route::get('/posts/create', [PostController::class,'create']);
+
+Route::get('/posts/{post}/{category?}', [PostController::class,'show']);
+
+/*Route::get('/posts/{post}/{category}', function ($post, $category) {
+    return "Aquí se mostrará el post {$post} de la categoría {$category}";
 });
+*/
